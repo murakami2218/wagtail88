97947514be0ccba03b07aad9a8fe0ddcdb84c794
murakami88 <murakami2218@gmail.com>
2019-02-18 21:35:37 +0900

    テキストコメントアウト

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.id))
+        TextSendMessage(text='きれいな花ですね(*^_^*)'))
 
 if __name__ == "__main__":
     app.run()
